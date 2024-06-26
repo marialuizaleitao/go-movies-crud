4c921bcfebfc4fb980458e319e106346d92e7e18
Maria Leitão <malufln.99@gmail.com>
2024-06-26 19:01:11 -0300

    Routes declarations

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
 type Movie struct {
 	ID        string    `json:"id"`
 	Isbn      string    `json:"isbn"`
@@ -13,3 +20,16 @@ type Director struct {
 }
 
 type Movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovies).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovies).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovies).Methods("DELETE")
+
+	fmt.Println("Starting server on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
