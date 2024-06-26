df2672133254d556690a45ab49517985d99185e7
Maria Leitão <malufln.99@gmail.com>
2024-06-26 18:52:02 -0300

    Movie struct

--- main.go
+++ main.go
@@ -1,1 +1,8 @@
 package main
+
+type Movie struct {
+	ID        string    `json:"id"`
+	Isbn      string    `json:"isbn"`
+	Title     string    `json:"title"`
+	Directors *Director `json:"director"`
+}
