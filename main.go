efb181a49c72f5a091992ec0c13b976fd794de60
Maria Leitão <malufln.99@gmail.com>
2024-06-27 13:37:49 -0300

    Implement createMovie function to add new movies with random IDs

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 type Movie struct {
@@ -39,6 +41,15 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Movie not found", http.StatusNotFound)
 }
 
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -46,9 +57,9 @@ func main() {
 	movies = append(movies, Movie{ID: "2", Genre: "Thriller", Title: "Psycho", Directors: &Director{FirstName: "Alfred", LastName: "Hitchcock"}})
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	r.HandleFunc("/movies", createMovies).Methods("POST")
-	r.HandleFunc("/movies/{id}", updateMovies).Methods("PUT")
-	r.HandleFunc("/movies/{id}", deleteMovies).Methods("DELETE")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Println("Starting server on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
